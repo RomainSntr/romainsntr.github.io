2bb6b850fee34e9f4b4ff96d2e3491e30a330f79
Artamis <artamis@hackademint.org>
2022-07-09 19:21:31 +0200

    Initial commit.

---
+++ script/index.js
@@ -0,0 +1,11 @@
+new fullpage("#fullpage", {
+  autoScrolling: true,
+  scrollHorizontally: true,
+  navigation: true,
+  navigationTooltips: ["Welcome", "Who am I ?", "Projects", "Art gallery"],
+  credits: {
+    enabled: false,
+    label: "Made with fullPage.js",
+    position: "right",
+  },
+});
